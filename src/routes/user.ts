827d854708fac71b8f61e7d6fb454de42c6f0a0f
Manuel Astudillo <manuel@optimalbits.com>
2021-11-11 22:46:55 +0800

    feat: add some more functionality to user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,6 +5,7 @@ import { StatusCodes } from "http-status-codes";
 import { Request, Response, Router, json } from "express";
 
 import { User } from "../models/user";
+import { db } from "../models";
 import { asyncWrap, canWrite, isRoot } from "../middleware";
 
 export const router = Router();
@@ -17,22 +18,6 @@ export const router = Router();
 */
 
 /**
- * Login?
- */
-router.post(
-  "/-/v1/login",
-  json(),
-  canWrite(),
-  asyncWrap(async (req: Request, res: Response) => {
-    // Unsure what we are supposed to do in this endpoint
-
-    res.status(StatusCodes.UNAUTHORIZED).json({
-      error: "You must be logged in to publish packages.",
-    });
-  })
-);
-
-/**
  * Update or add a new User
  *
  */
@@ -42,14 +27,14 @@ router.put(
   canWrite(),
   isRoot(),
   asyncWrap(async (req: Request, res: Response) => {
-    // Currently we do not use the scope, left for reference
+    // Currently we do not use the scope, left here for reference.
     const { "npm-scope": scope } = req.headers;
 
     const { name, password, email, type } = req.body;
     const { user } = res.locals;
 
-    const { userId } = res.locals.user;
-    const targetUserId = req.params.userId || userId;
+    const { _id: userId } = res.locals.user;
+    const targetUserId = req.params.userId;
 
     const data = {
       name,
@@ -80,7 +65,26 @@ router.put(
       }
       res.status(StatusCodes.OK).json({});
     } else if (res.locals.isRoot) {
-      const user = await User.create({ _id: targetUserId, ...data });
+      const transaction = await db.transaction();
+      try {
+        const user = await User.findOne({
+          where: { _id: targetUserId },
+          transaction,
+        });
+        if (user) {
+          res
+            .status(StatusCodes.CONFLICT)
+            .json({ ok: false, message: "cannot create existing user" });
+          throw new Error("Cannot create existing user");
+        }
+
+        await User.create({ _id: targetUserId, ...data }, { transaction });
+
+        await transaction.commit();
+      } catch (err) {
+        await transaction.rollback();
+        throw err;
+      }
       res.status(StatusCodes.CREATED).json({});
     } else {
       res.status(StatusCodes.FORBIDDEN).json({
@@ -91,11 +95,48 @@ router.put(
 );
 
 /**
+ * Change password
+ */
+router.post(
+  "/-/npm/v1/user",
+  json(),
+  canWrite(),
+  asyncWrap(async (req: Request, res: Response) => {
+    const { password } = req.body as { password: { old: string; new: string } };
+    const user = res.locals.user as User;
+
+    if (user.checkPassword(password.old)) {
+      await User.update(
+        {
+          password: password.new,
+        },
+        {
+          where: { _id: user.getDataValue("_id") },
+        }
+      );
+      res.status(StatusCodes.OK).json({});
+    } else {
+      res.status(StatusCodes.NOT_ACCEPTABLE).end();
+    }
+  })
+);
+
+/**
+ * Get public user data
+ */
+router.get(
+  "/-/npm/v1/user",
+  asyncWrap(async (req: Request, res: Response) => {
+    const { _id, password, ...user } = res.locals.user;
+    res.json(user);
+  })
+);
+
+/**
  * List all packages that this user has access to.
  */
 router.get(
   "/-/user/:user/package",
-  json(),
   asyncWrap(async (req: Request, res: Response) => {
     const { user } = req.params;
 
